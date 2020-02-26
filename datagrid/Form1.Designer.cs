dd293dad220e49f97cbd59787eddf14b6efe1cc7
Rohit Chapagain <rohitchapagain504@gmail.com>
2020-02-26 17:51:19 +0545

    desktopapp

---
+++ datagrid/Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace datagrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtemail = new System.Windows.Forms.TextBox();
+            this.gvuser = new System.Windows.Forms.DataGridView();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelete = new System.Windows.Forms.Button();
+            this.btnsave = new System.Windows.Forms.Button();
+            this.txtsearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gvuser)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(12, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Email";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Vivaldi", 32F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Purple;
+            this.label3.Location = new System.Drawing.Point(167, -1);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(133, 51);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Login ";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(102, 64);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(333, 26);
+            this.txtusername.TabIndex = 3;
+            // 
+            // txtemail
+            // 
+            this.txtemail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtemail.Location = new System.Drawing.Point(102, 104);
+            this.txtemail.Name = "txtemail";
+            this.txtemail.Size = new System.Drawing.Size(333, 26);
+            this.txtemail.TabIndex = 4;
+            // 
+            // gvuser
+            // 
+            this.gvuser.AllowUserToAddRows = false;
+            this.gvuser.AllowUserToDeleteRows = false;
+            this.gvuser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvuser.Location = new System.Drawing.Point(16, 208);
+            this.gvuser.Name = "gvuser";
+            this.gvuser.ReadOnly = true;
+            this.gvuser.Size = new System.Drawing.Size(459, 181);
+            this.gvuser.TabIndex = 6;
+            this.gvuser.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.Gvuser_RowHeaderMouseClick);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnupdate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnupdate.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.btnupdate.FlatAppearance.BorderSize = 2;
+            this.btnupdate.FlatAppearance.CheckedBackColor = System.Drawing.Color.Maroon;
+            this.btnupdate.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnupdate.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnupdate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.ForeColor = System.Drawing.Color.White;
+            this.btnupdate.Location = new System.Drawing.Point(222, 153);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(91, 34);
+            this.btnupdate.TabIndex = 7;
+            this.btnupdate.Text = "Update";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.Btnupdate_Click);
+            // 
+            // btndelete
+            // 
+            this.btndelete.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btndelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btndelete.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.btndelete.FlatAppearance.BorderSize = 2;
+            this.btndelete.FlatAppearance.CheckedBackColor = System.Drawing.Color.Maroon;
+            this.btndelete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btndelete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btndelete.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btndelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelete.ForeColor = System.Drawing.Color.White;
+            this.btndelete.Location = new System.Drawing.Point(339, 153);
+            this.btndelete.Name = "btndelete";
+            this.btndelete.Size = new System.Drawing.Size(85, 34);
+            this.btndelete.TabIndex = 8;
+            this.btndelete.Text = "Delete";
+            this.btndelete.UseVisualStyleBackColor = false;
+            this.btndelete.Click += new System.EventHandler(this.Btndelete_Click);
+            // 
+            // btnsave
+            // 
+            this.btnsave.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnsave.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnsave.FlatAppearance.BorderColor = System.Drawing.Color.Purple;
+            this.btnsave.FlatAppearance.BorderSize = 2;
+            this.btnsave.FlatAppearance.CheckedBackColor = System.Drawing.Color.Maroon;
+            this.btnsave.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.btnsave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnsave.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnsave.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsave.ForeColor = System.Drawing.Color.White;
+            this.btnsave.Location = new System.Drawing.Point(112, 153);
+            this.btnsave.Name = "btnsave";
+            this.btnsave.Size = new System.Drawing.Size(91, 34);
+            this.btnsave.TabIndex = 9;
+            this.btnsave.Text = "Save";
+            this.btnsave.UseVisualStyleBackColor = false;
+            this.btnsave.Click += new System.EventHandler(this.Btnsave_Click);
+            // 
+            // txtsearch
+            // 
+            this.txtsearch.Location = new System.Drawing.Point(335, 24);
+            this.txtsearch.Name = "txtsearch";
+            this.txtsearch.Size = new System.Drawing.Size(100, 20);
+            this.txtsearch.TabIndex = 10;
+            this.txtsearch.TextChanged += new System.EventHandler(this.Txtsearch_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(291, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Search";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(487, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtsearch);
+            this.Controls.Add(this.btnsave);
+            this.Controls.Add(this.btndelete);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.gvuser);
+            this.Controls.Add(this.txtemail);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gvuser)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtemail;
+        private System.Windows.Forms.DataGridView gvuser;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelete;
+        private System.Windows.Forms.Button btnsave;
+        private System.Windows.Forms.TextBox txtsearch;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ datagrid/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace datagrid
+{
+    public partial class Form1 : Form
+    {
+        int ID = 0;//use for the delete and update the data in datagrid and server
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)//to insert the value which have been inserted by the user into the textbox filled the result can been seen inside the datagridview
+        {
+            string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cm = new SqlCommand())
+                {
+                    cm.Connection = con;
+                    cm.CommandType = CommandType.Text;
+
+                    cm.CommandText = "select * from employee";
+                    DataTable dt = new DataTable();
+                    using (SqlDataAdapter da = new SqlDataAdapter(cm))
+                    {
+                        da.Fill(dt);
+                        gvuser.DataSource = dt;
+
+                    }
+                }
+            }
+
+        }
+        public void Result()//to view the result in datagridview at the same time when the save button is clicked
+        {
+            string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cm = new SqlCommand())
+                {
+                    cm.Connection = con;
+                    cm.CommandType = CommandType.Text;
+
+                    cm.CommandText = "select * from employee";
+                    DataTable dt = new DataTable();
+                    using (SqlDataAdapter da = new SqlDataAdapter(cm))
+                    {
+                        da.Fill(dt);
+                        gvuser.DataSource = dt;
+
+
+                    }
+                }
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void Btnsave_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void Btnupdate_Click(object sender, EventArgs e)//update the data
+        {
+            string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = con;
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.CommandText = "update employee set Username=@username,Email=@email where ID=@id";
+                    cmd.Parameters.AddWithValue("@id", ID);
+                    cmd.Parameters.AddWithValue("@username", txtusername.Text);
+                    cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    MessageBox.Show("Data have been update sucessfully!");
+                    Result();
+                    ID = 0;
+                    txtusername.Text = " ";
+                    txtemail.Text = " ";
+
+
+                }
+            }
+        }
+
+        private void Btnsave_Click(object sender, EventArgs e)//save the record insert by the users
+        {
+            if (txtusername.Text != " " && txtemail.Text != " ")
+            {
+                string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = CommandType.Text;
+
+                        cmd.CommandText = "insert into employee (username, email)values(@username,@email)";
+                        cmd.Parameters.AddWithValue("@username", txtusername.Text);
+                        cmd.Parameters.AddWithValue("@email", txtemail.Text);
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        MessageBox.Show("Data have been inserted sucessfully");
+                        txtusername.Text = " ";
+                        txtemail.Text = " ";
+                        Result();
+                    }
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("you must fill up the user name and password!");
+            }
+
+        }
+
+        private void Btndelete_Click(object sender, EventArgs e)//delete record
+        {
+            if (ID != 0)
+            {
+                string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+                using (SqlConnection con = new SqlConnection(connectionstring))
+                {
+                    con.Open();
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        cmd.Connection = con;
+                        cmd.CommandType = CommandType.Text;
+
+                        cmd.CommandText = "Delete employee where ID=@id";
+                        cmd.Parameters.AddWithValue("@id", ID);
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                        MessageBox.Show("Data has been delete sucessfully!");
+                        Result();
+                        txtusername.Text = " ";
+                        txtemail.Text = " ";
+                        ID = 0;
+                    }
+
+                }
+            }
+        }
+
+        private void Gvuser_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            ID = Convert.ToInt32(gvuser.Rows[e.RowIndex].Cells[0].Value.ToString());
+            txtusername.Text = gvuser.Rows[e.RowIndex].Cells[1].Value.ToString();
+            txtemail.Text = gvuser.Rows[e.RowIndex].Cells[2].Value.ToString();
+        }
+
+        private void Txtsearch_TextChanged(object sender, EventArgs e)//serach text box
+        {
+
+            string connectionstring = "data source=DESKTOP-G5EM3AS ;initial catalog=ismt;integrated security=true";
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                con.Open();
+                using (SqlCommand cm = new SqlCommand())
+                {
+                    cm.Connection = con;
+                    cm.CommandType = CommandType.Text;
+                    cm.CommandText = "select * from employee where Username like  '" + txtsearch.Text + "%'";
+                    DataTable dt = new DataTable();
+                    using (SqlDataAdapter da = new SqlDataAdapter(cm))
+                    {
+                        da.Fill(dt);
+                        gvuser.DataSource = dt;
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+    
+
+
+
